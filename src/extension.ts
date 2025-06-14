dae81dad86f641090f82d30d3141e8148bf28204
xinchen <xinchentechnote@163.com>
2025-06-14 11:41:32 +0800

    feat:registry base command

--- src/extension.ts
+++ src/extension.ts
@@ -10,16 +10,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "fin-proto-vscode" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('fin-proto-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fin-proto-vscode!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('fin-proto-vscode.format', () => {
+		vscode.window.showInformationMessage('formatting');
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand('fin-proto-vscode.compile', () => {
+		vscode.window.showInformationMessage('compiling');
+	}));
 }
 
 // This method is called when your extension is deactivated
