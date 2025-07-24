d9c932a6ddb778f4c76777f4a2b87adf1468d1f1
xinchen <xinchentechnote@163.com>
2025-07-24 23:38:15 +0800

    feat:check duplicate packet

--- src/parser/model.ts
+++ src/parser/model.ts
@@ -3,8 +3,8 @@ import { ANTLRErrorListener, Token } from "antlr4ts";
 export class DslSyntaxError {
   constructor(
     public line: number,
-    public start: number,
-    public end: number,
+    public startIndex: number,
+    public stopIndex: number,
     public msg: string,
   ) { }
 }
@@ -25,24 +25,54 @@ export class SyntaxErrorCollector implements ANTLRErrorListener<Token> {
 }
 
 export class BinaryModel {
+  rootPacket?: Packet;
   metaDataMap = new Map<string, MetaData>();
   options = new Map<string, string>();
   packets: Packet[] = [];
+  packetMaps = new Map<string, Packet>();
   syntaxErrors: DslSyntaxError[] = [];
-  rootPacket?: Packet;
+
+  addSyntaxError(error: DslSyntaxError) {
+    this.syntaxErrors.push(error);
+  }
 
   addMetaData(meta: MetaData) {
+    // if (this.metaDataMap.has(meta.name)) {
+    //   this.addSyntaxError(
+    //     new DslSyntaxError(line, start, end, `Duplicate metadata definition: ${meta.name}`)
+    //   );
+    //   return;
+    // }
     this.metaDataMap.set(meta.name, meta);
   }
 
-  addOption(name: string, value: string) {
+  addOption(name: string, value: string, line: number, startIndex: number, stopIndex: number) {
+    if (this.options.has(name)) {
+      this.addSyntaxError(
+        new DslSyntaxError(line, startIndex, stopIndex, `Duplicate option definition: ${name}`)
+      );
+      return;
+    }
     this.options.set(name, value);
   }
 
   addPacket(packet: Packet) {
+    if (this.packetMaps.has(packet.name)) {
+      this.addSyntaxError(
+        new DslSyntaxError(packet.line, packet.startIndex, packet.stopIndex, `Duplicate packet definition: ${packet.name}`)
+      );
+      return;
+    }
+    this.packetMaps.set(packet.name, packet);
     this.packets.push(packet);
     if (packet.isRoot) {
-      this.rootPacket = packet;
+      if (this.rootPacket) {
+        this.addSyntaxError(
+          new DslSyntaxError(packet.line, packet.startIndex, packet.stopIndex, `Root packet already defined: ${this.rootPacket.name}. New root packet: ${packet.name}`)
+        );
+      } else {
+        this.rootPacket = packet;
+      }
     }
   }
 
@@ -51,25 +81,43 @@ export class BinaryModel {
   }
 }
 
-export class MetaData {
+export class Positioned {
+  constructor(
+    public line: number = 0,
+    public startIndex: number = 0,
+    public stopIndex: number = 0
+  ) { }
+}
+
+export class MetaData extends Positioned {
   constructor(
     public name: string,
     public type: string,
     public basicType: string,
-    public description: string
-  ) { }
+    public description: string,
+    line: number = 0,
+    startIndex: number = 0,
+    stopIndex: number = 0
+  ) {
+    super(line, startIndex, stopIndex);
+  }
 }
 
-export class Packet {
+export class Packet extends Positioned {
   constructor(
     public name: string,
     public isRoot: boolean,
     public fields: Field[],
-    public matchFields: Record<string, MatchPair[]> = {}
-  ) { }
+    public matchFields: Record<string, MatchPair[]> = {},
+    line: number = 0,
+    startIndex: number = 0,
+    stopIndex: number = 0
+  ) {
+    super(line, startIndex, stopIndex);
+  }
 }
 
-export class Field {
+export class Field extends Positioned {
   constructor(
     public name: string,
     public type: string,
@@ -78,10 +126,17 @@ export class Field {
     public inerObject?: Packet,
     public doc: string = "",
     public matchType?: string,
-    public matchPairs: MatchPair[] = []
-  ) { }
+    public matchPairs: MatchPair[] = [],
+    line: number = 0,
+    startIndex: number = 0,
+    stopIndex: number = 0
+  ) {
+    super(line, startIndex, stopIndex);
+  }
 }
 
-export class MatchPair {
-  constructor(public key: string, public value: string) { }
+export class MatchPair extends Positioned {
+  constructor(public key: string, public value: string, line: number = 0, startIndex: number = 0, stopIndex: number = 0) {
+    super(line, startIndex, stopIndex);
+  }
 }
